18625ed1f0711bd3e8571fc72ad320a1b79bb853
Phuoc Do <dnprock@gmail.com>
2014-12-16 18:39:30 -0800

    Load data

--- vida_template.js
+++ vida_template.js
@@ -4,16 +4,23 @@ $(document).ready(function() {
     window.config = manifest.properties;
     
     // load HTML and append to body canvas
-    $.get('document.html', function(html) {
+    var document_html = manifest.html[0],
+        document_css = manifest.stylesheet[0],
+        document_js = manifest.javascript[0],
+        document_data = manifest.data[0]
+      
+    $.get(document_html, function(html) {
       $('#canvas').append(html);
       
-      // load CSS
-      var css = $('<link rel="stylesheet" type="text/css" href="document.css" />');
-      $('body').append(css);
+      $.get(document_data, function(data) {
+        // load CSS
+        var css = $('<link rel="stylesheet" type="text/css" href="' + document_css + '" />');
+        $('body').append(css);
       
-      // load Javascript
-      var script = $('<script type="text/javascript" src="document.js"></script>');
-      $('body').append(script);
+        // load Javascript
+        var script = $('<script type="text/javascript" src="' + document_js +'"></script>');
+        $('body').append(script);
+      })
     });
   });
   
